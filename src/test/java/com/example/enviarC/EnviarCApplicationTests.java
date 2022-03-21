55ccfa047ccfb8c5313a30daf7841b2dfef8e304
stivengomezg <42156887+stivengomezg@users.noreply.github.com>
2022-03-21 11:08:19 -0500

    primer comit

---
+++ src/test/java/com/example/enviarC/EnviarCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enviarC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviarCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
